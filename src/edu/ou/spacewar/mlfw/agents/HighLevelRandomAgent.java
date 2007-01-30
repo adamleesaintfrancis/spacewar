cf07d2eec91adbf22c097586ffa555c4e1262a1e
amarack <amarack@db1278d6-291b-0410-9b54-55d16e500c56>
2007-01-30 08:13:29 +0000

    Added a high level random agent. Currently it works correctly, but does not yet draw its goal and path on the screen. That will come... soon!

---
+++ src/edu/ou/spacewar/mlfw/agents/HighLevelRandomAgent.java
@@ -0,0 +1,71 @@
+package edu.ou.spacewar.mlfw.agents;
+
+import java.io.File;
+import java.util.*;
+import edu.ou.mlfw.*;
+import edu.ou.utils.*;
+import edu.ou.spacewar.ImmutableSpacewarState;
+import edu.ou.spacewar.objects.immutables.*;
+import edu.ou.spacewar.objects.ShipNavigationActions;
+/**
+ * HighLevelRandomAgent is a simple high level agent that chooses a random location (not in an obstacle) 
+ * and moves to that location using the custom built high level actions and environment.
+ */
+public class HighLevelRandomAgent implements Agent {
+private final Random random = new Random();
+
+/**
+ * The agent receives the current state (which should be cast to ImmutableSpacewarState 
+ * to make it more useful, and a set of available actions. Normally these actions are simply the set of 
+ * legal ShipCommands to make (see the other random agent), in this case we have a custom environment which 
+ * sends a single action, from which two types of actions can be created. The first is to do nothing. The 
+ * second is to move to some point in space, specified by the x and y locations.
+ * This agent then randomly chooses x and y location to move to, and returns the resulting action.
+ * 
+ * This illustrates how custom Action classes, Environment classes, and Agent classes can be used to 
+ * provide different features to the agent. Here the agent does not need to worry about the implementation 
+ * of low level actions to move from one spot to another. Instead it relies on the Environment, and the 
+ * provided HighLevelActions class.
+ * 
+ */
+public Action startAction(State state, Set<Action> actions) {
+  if (actions.size() == 0)
+    return null;
+  ShipNavigationActions navAct = (ShipNavigationActions)actions.toArray()[0];
+  
+  int fixed_multiplier = 1000;
+  boolean goalIsClear = true;
+  Vector2D pos = ((ImmutableSpacewarState)state).getShips()[0].getPosition();
+  
+  float dirX;
+  float dirY;
+  float newX;
+  float newY;
+  Vector2D goalPos = null;
+  while(goalPos == null) {
+      dirX = random.nextFloat() - (float)0.5;
+      dirY = random.nextFloat() - (float)0.5;
+      newX = dirX*fixed_multiplier + pos.getX();
+      newY = dirY*fixed_multiplier + pos.getY();
+      goalPos = new Vector2D(newX, newY);
+  
+      ImmutableObstacle[] obs = ((ImmutableSpacewarState)state).getObstacles();
+      for(int i=0; i<obs.length; i++)
+        if(obs[0].getPosition().equals(goalPos))
+          {
+            goalPos = null;
+            break;
+          }
+      }
+  return navAct.MoveToPoint(goalPos.getX(), goalPos.getY());
+
+}
+
+public void endAction(State state) {
+//do nothing
+}
+
+public void initialize(File configfile) {
+//no configuration necessary
+}
+}
---
+++ src/edu/ou/spacewar/mlfw/environments/HighLevelEnvironment.java
@@ -0,0 +1,165 @@
+
+package edu.ou.spacewar.mlfw.environments;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.ou.spacewar.simulator.*;
+import edu.ou.mlfw.*;
+import edu.ou.utils.*;
+import edu.ou.spacewar.*;
+import edu.ou.spacewar.objects.immutables.*;
+import edu.ou.spacewar.objects.*;
+
+
+public class HighLevelEnvironment implements Environment {
+  
+  // Store the current state each time it gets received
+  private ImmutableSpacewarState envState = null;
+  // Store the current action being executed over multiple timesteps
+  private ShipNavigationActions currentAction = null;
+  
+  /**
+   * This receives the set of legal actions from the world. For us, 
+   * this is a set of legal ShipCommand actions for moving the ship around. 
+   * This method returns the set of actions an agent has access too. In this situation 
+   * we only send one action, from which the possible high level actions can be created.
+   * 
+   */
+  public Set<Action> getAgentActions(Set<Action> legalActions) {
+    if(currentAction != null)
+      return Collections.unmodifiableSet(new HashSet<Action>());
+    
+    Set<Action> agentActions = Collections.unmodifiableSet(
+          new HashSet<Action>(Arrays.asList(new ShipNavigationActions(
+                  ((ImmutableSpacewarState)envState).getWidth(),((ImmutableSpacewarState)envState).getHeight()))));
+   return agentActions; 
+  }
+
+
+  /**
+   * When the world sends the state, we can then change it before sending it to the agent. This allows us 
+   * to provide the agent with a customized state (perhaps smaller or abstracted in some way). We do not 
+   * make use of this feature in this class, but we do store the state for later use.
+   */
+  public State getAgentState(State state) {
+    envState = (ImmutableSpacewarState)state;
+    
+    return state;
+  }
+
+  /**
+   * This takes an action that the agent can execute, and converts it into the actions the world understands. 
+   * Here that means we are taking the high level actions and translating them into ShipCommands, returning 
+   * one ShipCommand at a time. 
+   */
+  public Action getControllableAction(Action action) {
+    /* Translate the high level action of DoNothing or MoveToPoint into low level actions */
+    if(currentAction == null)
+      currentAction = (ShipNavigationActions)action;
+    
+    if(currentAction == null || currentAction.isDoNothing())
+      return ShipCommand.DoNothing;
+      
+    return moveToPoint(envState, new Vector2D(currentAction.getX(), currentAction.getY()));
+  }
+ 
+  public void initialize(File configfile) {
+    //do nothing
+  }
+
+  // we want actions like: MoveToPoint
+
+  public ShipCommand moveToPoint(ImmutableSpacewarState state, Vector2D goalPosition)
+  {
+    /* This is a temporary hack, I am deeply sorry. 
+     * Here we assume only one ship, and that it is ours. Later we will use 
+     * attributes of the ships to identify which we are able to control.
+     */
+    ImmutableShip myShip = state.getShips()[0];
+    Vector2D currentPosition = myShip.getPosition();
+    Vector2D pathToGoal = Space.findShortestDistance(currentPosition, goalPosition, state.getWidth(), state.getHeight());
+ 
+    // If we are close to the goal point lets just say we are there
+    if(pathToGoal.getMagnitude() < 10)
+      {
+        // Close enough, we are finished executing this extended high level action
+        currentAction = null;
+        return ShipCommand.DoNothing;
+      }
+    
+    // this is the velocity that the ship is trying to hit the goal at
+    // it should also never exceed this speed
+    // can tune this parameter
+    Vector2D maxVelocity;
+    float maxVel = 7f;
+
+    // account for which quandrant the goal is in
+    if (pathToGoal.getX() > 0) {
+        if (pathToGoal.getY() > 0) {
+            maxVelocity = new Vector2D(maxVel, maxVel);
+        } else {
+            maxVelocity = new Vector2D(maxVel, -maxVel);
+        }
+    } else {
+        if (pathToGoal.getY() > 0) {
+            maxVelocity = new Vector2D(-maxVel, maxVel);
+        } else {
+            maxVelocity = new Vector2D(-maxVel, -maxVel);
+        }
+    }
+
+    // get the PD control vector to the goal
+    Vector2D accel = pdControl(state, goalPosition, currentPosition,
+            maxVelocity, myShip.getVelocity());
+
+    // if it is small enough (can tune this parameter), just drift as we are close
+    if (accel.getMagnitude() < 6) {
+        return ShipCommand.DoNothing;
+    } else {
+        // what is the angle between the acceleration and my current heading?
+        float oriangle = accel.angleBetween(myShip.getOrientation());
+
+        // can tune this parameter
+        if (Math.abs(oriangle) < (Math.PI / 16)) {
+          return ShipCommand.Thrust;
+        } else if (oriangle > 0) {
+          return ShipCommand.TurnLeft;
+        } else {
+          return ShipCommand.TurnRight;
+        }
+    }
+
+    
+  }
+  
+    /**
+     * Proportional derivative vector controller
+     * @param goalLoc location that you are trying to get to
+     * @param currentLoc your current location
+     * @param goalVelocity velocity you want to achieve
+     * @param currentVelocity your current velocity
+     */
+    private Vector2D pdControl(ImmutableSpacewarState state, Vector2D goalLoc, Vector2D currentLoc,
+                               Vector2D goalVelocity, Vector2D currentVelocity) {
+        float Kp, Kv;
+
+        // tuneable parameters but they should be in relationships to one another
+        Kv = 0.8f;
+        Kp = 0.16f;
+
+        // take care of wraparound
+        Vector2D position = (state.findShortestDistance(currentLoc, goalLoc)).multiply(Kp);
+        Vector2D vel = (goalVelocity.subtract(currentVelocity)).multiply(Kv);
+
+        Vector2D accel = position.add(vel);
+
+        return accel;
+    }
+
+
+
+}
