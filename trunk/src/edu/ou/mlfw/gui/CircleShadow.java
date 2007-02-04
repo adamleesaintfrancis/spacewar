3565580355ef89ca134dbbfb0c25a81c6adc750f
dramymcgovern <dramymcgovern@db1278d6-291b-0410-9b54-55d16e500c56>
2007-02-04 19:34:03 +0000

    Added a circle shape shadow that can be set to any color.-Amy

---
+++ trunk/src/edu/ou/mlfw/gui/CircleShadow.java
@@ -0,0 +1,52 @@
+package edu.ou.mlfw.gui;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+import edu.ou.mlfw.gui.Shadow2D;
+import edu.ou.spacewar.objects.Beacon;
+import edu.ou.utils.Vector2D;
+
+/**
+ * The default shadow class for a Beacon.
+ */
+public class CircleShadow extends Shadow2D {
+    private Color color = Color.RED;
+    private static final int radius = 5;
+    private Vector2D location;
+    boolean drawMe;
+    
+    public CircleShadow(Vector2D location) {
+        super(radius * 2, radius * 2);
+        this.location = location;
+    }
+
+    public Vector2D getRealPosition() {
+        return location;
+    }
+
+    public boolean drawMe() {
+        return drawMe;
+    }
+    
+    public void setDrawMe(boolean b) {
+    	drawMe = b;
+    }
+
+    public void draw(Graphics2D g) {
+        Ellipse2D.Float shape = new Ellipse2D.Float(drawposition.getX() - radius,
+                drawposition.getY() - radius, 2 * radius, 2 * radius);
+
+        g.setColor(color);
+        g.fill(shape);
+        g.draw(shape);
+    }
+    
+    public void setColor(Color newColor) {
+    	color = newColor;
+    }
+
+    public void cleanUp() {
+        //do nothing...
+    }
+}
