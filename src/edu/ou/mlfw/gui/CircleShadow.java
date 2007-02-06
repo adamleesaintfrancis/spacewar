e551fbaa1cb2c29934a159cde27b8d2a5909df5a
jfager <jfager@db1278d6-291b-0410-9b54-55d16e500c56>
2007-02-06 03:35:45 +0000

    updated ant build to set headless=true for non-gui mode

--- src/edu/ou/mlfw/gui/CircleShadow.java
+++ src/edu/ou/mlfw/gui/CircleShadow.java
@@ -10,9 +10,9 @@ import edu.ou.utils.Vector2D;
  * The default shadow class for a Beacon.
  */
 public class CircleShadow extends Shadow2D {
-    private Color color = Color.RED;
     private static final int radius = 5;
     private Vector2D location;
+    private Color color = Color.RED;
     boolean drawMe;
     
     public CircleShadow(Vector2D location) {
