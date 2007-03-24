885efbf9ab49e3007c6af9d94bf262f92c6a226b
eagle1701e <eagle1701e@db1278d6-291b-0410-9b54-55d16e500c56>
2007-03-24 05:45:51 +0000

    Still fixing things

---
+++ src/edu/ou/spacewar/mlfw/agents/RandomAgent.java
@@ -0,0 +1,40 @@
+package edu.ou.spacewar.mlfw.agents;
+
+import java.io.File;
+import java.util.*;
+
+import edu.ou.mlfw.*;
+
+/**
+ * RandomAgent is a simple general agent that returns a random action at every timestep.
+ */
+public class RandomAgent implements Agent {
+	private final Random random = new Random();
+	public Action startAction(State state, Set<Action> actions) {
+		if (actions.size() == 0)
+            return null;
+        int randomIndex = random.nextInt(actions.size());
+        return (Action)actions.toArray()[randomIndex];
+	}
+
+	public void endAction(State state) {
+		//do nothing		
+	}
+
+	public void initialize(File configfile) {
+		//no configuration necessary
+	}
+
+	public void loadData(File datafile) {
+		// do nothing
+	}
+	
+	public void setControllableName(String name) {
+		// random agent, do nothing.
+	}
+
+	public void shutdown() {
+		// do nothing
+	}
+	
+}
