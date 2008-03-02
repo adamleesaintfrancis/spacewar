fa310432a541e05a0e23b1013e67d00f8114bdb0
b1mck1 <b1mck1@db1278d6-291b-0410-9b54-55d16e500c56>
2008-03-02 22:20:28 +0000

    updated to match changes in abstractshipclient

--- trunk/examples/random/SampleRandomClient.java
+++ trunk/examples/random/SampleRandomClient.java
@@ -85,18 +85,6 @@ public class SampleRandomClient extends AbstractShipClient implements Drawer {
 		myName = name;
 	}
 
-	private ImmutableShip findMyShip(ImmutableSpacewarState state) {
-		if (myName == null) {
-			return null;
-		}
-		for (ImmutableShip s : state.getShips()) {
-			if (s.getName().equals(myName)) {
-				return s;
-			}
-		}
-		return null;
-	}
-
 	/**
 	 * Ensures that the specified point is not inside an obstacle
 	 * 
@@ -127,18 +115,17 @@ public class SampleRandomClient extends AbstractShipClient implements Drawer {
 	 * action.
 	 */
 	public ShipCommand startAction(ImmutableSpacewarState state,
-			ControllableShip controllable) {
+			ImmutableShip myShip) {
 		
 		int fixed_multiplier = 1000;
 		// boolean goalIsClear = true;
-
-		ImmutableShip myShip = findMyShip(state); 
+		
 		Vector2D pos = myShip.getPosition();
 		// save some information for drawing the line to the goal
 		startPosition = pos;
 		makeNewLine = true;
 		
-		if (pickNewPoint) {
+		if (reachedGoal()) {
 			float dirX;
 			float dirY;
 			float newX;
@@ -164,109 +151,13 @@ public class SampleRandomClient extends AbstractShipClient implements Drawer {
 		}
 		lineVec = state.findShortestDistance(startPosition, goalPos);
 		
-		return moveToPoint(state, goalPos);
+		return moveToPoint(goalPos);
 	}
 
-	// we want actions like: MoveToPoint
-	public ShipCommand moveToPoint(ImmutableSpacewarState state,
-			Vector2D goalPosition) {
-		/*
-		 * This is a temporary hack, I am deeply sorry. Here we assume only one
-		 * ship, and that it is ours. Later we will use attributes of the ships
-		 * to identify which we are able to control.
-		 */
-		ImmutableShip myShip = findMyShip(state);
-		Vector2D currentPosition = myShip.getPosition();
-		Vector2D pathToGoal = Space.findShortestDistance(currentPosition,
-				goalPosition, state.getWidth(), state.getHeight());
 
-		// If we are close to the goal point lets just say we are there
-		if (pathToGoal.getMagnitude() < 10) {
-			// Close enough, finish executing this extended high level action
-			pickNewPoint  = true;
-			return ShipCommand.DoNothing;
-		}
-
-		// this is the velocity that the ship is trying to hit the goal at
-		// it should also never exceed this speed
-		// can tune this parameter
-		Vector2D maxVelocity;
-		float maxVel = 7f;
-
-		// account for which quandrant the goal is in
-		if (pathToGoal.getX() > 0) {
-			if (pathToGoal.getY() > 0) {
-				maxVelocity = new Vector2D(maxVel, maxVel);
-			}
-			else {
-				maxVelocity = new Vector2D(maxVel, -maxVel);
-			}
-		}
-		else {
-			if (pathToGoal.getY() > 0) {
-				maxVelocity = new Vector2D(-maxVel, maxVel);
-			}
-			else {
-				maxVelocity = new Vector2D(-maxVel, -maxVel);
-			}
-		}
-
-		// get the PD control vector to the goal
-		Vector2D accel = pdControl(state, goalPosition, currentPosition,
-				maxVelocity, myShip.getVelocity());
-
-		// if it is small enough (can tune this parameter), just drift
-		if (accel.getMagnitude() < 6) {
-			return ShipCommand.DoNothing;
-		}
-		else {
-			// the angle between the acceleration and my current heading
-			float oriangle = accel.angleBetween(myShip.getOrientation());
-
-			// can tune this parameter
-			if (Math.abs(oriangle) < (Math.PI / 16)) {
-				return ShipCommand.Thrust;
-			}
-			else if (oriangle > 0) {
-				return ShipCommand.TurnLeft;
-			}
-			else {
-				return ShipCommand.TurnRight;
-			}
-		}
-	}
-
-	/**
-	 * Proportional derivative vector controller
-	 * 
-	 * @param goalLoc
-	 *                location that you are trying to get to
-	 * @param currentLoc
-	 *                your current location
-	 * @param goalVelocity
-	 *                velocity you want to achieve
-	 * @param currentVelocity
-	 *                your current velocity
-	 */
-	private Vector2D pdControl(ImmutableSpacewarState state, Vector2D goalLoc,
-			Vector2D currentLoc, Vector2D goalVel, Vector2D currentVel) {
-		// tunable parameters; should be in relationship to one another
-		// TODO: what relationship? Kp = 2Kv?
-		float Kv = 0.8f;
-		float Kp = 0.16f;
-
-		// take care of wrap-around
-		Vector2D shortdist = state.findShortestDistance(currentLoc, goalLoc);
-		Vector2D position = shortdist.multiply(Kp);
-		Vector2D vel = goalVel.subtract(currentVel).multiply(Kv);
-
-		Vector2D accel = position.add(vel);
-
-		return accel;
-	}
 
 	// do nothing methods
-	public void endAction(ImmutableSpacewarState s, ControllableShip c) {
+	public void endAction(ImmutableSpacewarState s, ImmutableShip c) {
 	}
 
 	public void initialize(File configfile) {
@@ -278,9 +169,4 @@ public class SampleRandomClient extends AbstractShipClient implements Drawer {
 	public void shutdown() {
 	}
 
-	@Override
-	public void setDisplayName(String name) {
-		this.myName = name;
-		super.setDisplayName(name);
-	}
 }
