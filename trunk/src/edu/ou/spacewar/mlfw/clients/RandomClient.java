424c966b66b255471c3d2e4f029e09bf632ce642
b1mck1 <b1mck1@db1278d6-291b-0410-9b54-55d16e500c56>
2008-03-02 21:57:13 +0000

    Changed all example agents to have startAction(ImmutableSpacewarState, ImmutableShip) rather than startAction(ImmutableSpacewarState, ControllableShip). This is because the getState member of the controllable ship returns only the original state of the ship.

--- trunk/src/edu/ou/spacewar/mlfw/clients/RandomClient.java
+++ trunk/src/edu/ou/spacewar/mlfw/clients/RandomClient.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.util.*;
 
 import edu.ou.spacewar.ImmutableSpacewarState;
-import edu.ou.spacewar.controllables.ControllableShip;
 import edu.ou.spacewar.objects.ShipCommand;
+import edu.ou.spacewar.objects.immutables.ImmutableShip;
 
 /**
  * RandomClient returns a random action at every timestep.
@@ -15,13 +15,13 @@ public class RandomClient extends AbstractShipClient {
 	private final ShipCommand[] commands = ShipCommand.getAllCommands();
 	
 	public ShipCommand startAction( ImmutableSpacewarState state,
-							        ControllableShip controllable ) 
+			ImmutableShip controllable ) 
 	{
 		return commands[ random.nextInt( commands.length ) ];
 	}
 
 	// do nothing methods
-	public void endAction(ImmutableSpacewarState s, ControllableShip c) {}
+	public void endAction(ImmutableSpacewarState s, ImmutableShip c) {}
 	public void initialize(File config) {}
 	public void loadData(File data) {}
 	public void shutdown() {}
