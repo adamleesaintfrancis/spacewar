8e0c31ea7f7d8635d050118ad9f76db0233cbd02
b1mck1 <b1mck1@db1278d6-291b-0410-9b54-55d16e500c56>
2008-03-02 21:58:44 +0000

    Implemented hashCode and equals(Object) to support putting vector2D objects in collections that depend on these methods. Note that equals(Object) is called for Collection.contains, not equals(vector2D).

--- src/edu/ou/utils/Vector2D.java
+++ src/edu/ou/utils/Vector2D.java
@@ -294,4 +294,29 @@ public final class Vector2D implements Comparable<Vector2D>, Cloneable {
             test2 = getRandom(rand, 20);
         }
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Vector2D other = (Vector2D) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
 }
